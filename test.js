f4a83f577a04b1bbae507128043ccd3f3b2c5c7e
Mohammad Zeineddine <mzeinedine@hotmail.com>
2023-11-21 19:26:58 +0200

    test2

---
+++ test.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+describe('App functionality tests', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('D:\Mawed\Semester 11 - Fall  2024\Distributed Systems\Midterm\index.html', { waitUntil: 'networkidle0' }); // replace with the path to your HTML file
+  });
+  
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test('Greet button changes text', async () => {
+    await page.type('#nameInput', 'Mohammad');
+    await page.click('#greetButton');
+    const greetingText = await page.$eval('#greetingText', el => el.textContent);
+    expect(greetingText).toBe('Hello, Mohammad! Welcome to the web app!');
+  });
+});
